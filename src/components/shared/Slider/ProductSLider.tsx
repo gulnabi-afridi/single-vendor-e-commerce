b9f30b2b2d2e6878ec89467ecbdb806e7882ab22
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-21 10:02:48 -0700

    created a shared slider component

---
+++ src/components/shared/Slider/ProductSLider.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Mousewheel, Autoplay } from "swiper";
+import ProductCard from "@/components/shared/ProductCards/ProductCard";
+
+import H1Heading from "@/components/shared/CustomTypography/H1Heading";
+
+interface Props {
+  data: any;
+  title: string;
+  slidesToShow?: number;
+}
+
+const ProductSlider: React.FC<Props> = ({ data, title, slidesToShow = 4 }) => {
+  return (
+    <div className="w-full flex flex-col gap-4 items-center justify-center">
+      <H1Heading>{title}</H1Heading>
+      <div className="h-[450px] max-w-full">
+        <Swiper
+          slidesPerView={slidesToShow}
+          spaceBetween={15}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            300: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 50,
+            },
+          }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[Pagination, Mousewheel, Autoplay]}
+          className="mySwiper"
+        >
+          {data.map((item: any, index: number) => (
+            <SwiperSlide key={index}>
+              <ProductCard
+                title={item.title}
+                price={item.price}
+                newArrival={item.new}
+                sale={item.sale}
+                soldout={item.soldOut}
+                picture={item.picture}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default ProductSlider;
