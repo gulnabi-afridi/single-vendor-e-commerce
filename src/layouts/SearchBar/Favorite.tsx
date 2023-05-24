dea3ab7e9b33df5f1748e42ce680dff01f3b7b3d
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-24 00:40:37 -0700

    finished wishlist dailogue

--- src/layouts/SearchBar/Favorite.tsx
+++ src/layouts/SearchBar/Favorite.tsx
@@ -2,27 +2,40 @@ import React from "react";
 import Badge from "@mui/material/Badge";
 import { MdFavoriteBorder } from "react-icons/md";
 import DialougeWrapper from "@/components/shared/DialogueWrapper/DialougeWrapper";
-interface Props{
-  open:boolean;
-  setOpen:any;
-  onClose:any;
+import { Data } from "../../../constants/Data/JSON";
+import WishListCard from "@/components/shared/ProductCards/WishListCard";
+
+interface Props {
+  open: boolean;
+  setOpen: any;
+  onClose: any;
 }
 
-const Favorite:React.FC<Props>=({open,setOpen,onClose}:Props)=> {
+const Favorite: React.FC<Props> = ({ open, setOpen, onClose }: Props) => {
   return (
     <div>
       <Badge badgeContent={4}>
-        <MdFavoriteBorder onClick={setOpen} className="text-[26px] sm:text-[30px] cursor-pointer" />
+        <MdFavoriteBorder
+          onClick={setOpen}
+          className="text-[26px] sm:text-[30px] cursor-pointer"
+        />
       </Badge>
-      <DialougeWrapper
-          open={open}
-          setState={onClose}
-          title="My Wishlist"
-        >
-          <div className="w-full md:w-[40%] flex flex-col items-center justify-center gap-12 mx-auto"></div>
-        </DialougeWrapper>
+      <DialougeWrapper open={open} setState={onClose} title="My Wishlist">
+        <div className="w-full lg:w-[80%] flex flex-col items-center justify-center gap-4 mx-auto overflow-auto">
+          {Data.WishListItems.map((item: any, index: number) => (
+            <WishListCard
+              key={index}
+              name={item.name}
+              picture={item.picture}
+              price={item.price}
+              status={item.status}
+              seeDetails={onClose}
+            />
+          ))}
+        </div>
+      </DialougeWrapper>
     </div>
   );
-}
+};
 
 export default Favorite;
